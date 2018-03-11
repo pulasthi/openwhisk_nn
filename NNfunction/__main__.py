338ded41f93ea22a84676dea02443dbd5e60fc6d
pulasthi <pulasthi911@gmail.com>
2018-03-11 18:07:52 -0400

    invoker can invoke NN trainning functions

--- NNfunction/__main__.py
+++ NNfunction/__main__.py
@@ -2,8 +2,13 @@ import mnist_loader
 import network
 
 def main(args):
+    sizes = args.get("layers", [784, 20, 10])
+    epochs = args.get("epochs", 5)
+    eta = args.get("eta", 3.0)
+    mini_batch_size = args.get("mini_batch_size", 10)
+
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-    net = network.Network([784, 30, 10])
-    net.SGD(training_data, 5, 10, 3.0, test_data=None)
+    net = network.Network(sizes)
+    net.SGD(training_data, epochs, mini_batch_size, eta, test_data=None)
     n_test = len(test_data)
     return {"Results": "Epoch Last: {0} / {1}".format(net.evaluate(test_data), n_test)}
--- invokerFunction/NNInvoker.py
+++ invokerFunction/NNInvoker.py
@@ -1,9 +1,14 @@
 import random
+import os
+import subprocess
 
 # Third-party libraries
 import numpy as np
 import couchdb
 import json
+import requests
+
+
 """
 This function is used to invoke the NN functions
 The parallizm that is needed is given to this function so that it will invoke the
@@ -33,9 +38,12 @@ class NNInvoker(object):
         self.iterations = args.get("iter", 10)
         self.parallelism = args.get("para", 4)
         self.currIter = args.get("curr", 0)
-        self.sizes = args.get("layers", [784, 3, 10])
-
+        self.sizes = args.get("layers", [784, 15, 10])
+        self.epochs = args.get("epochs", 1)
+        self.eta = args.get("eta", 3.0)
+        self.mini_batch_size = args.get("mini_batch_size", 10)
         #constants
+
         self.functionCount = 'NNFunctionCount'
         self.iterCount = 'iterCount'
         self.initw = 'initw'
@@ -64,6 +72,7 @@ class NNInvoker(object):
             self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
             self.weights = [np.random.randn(y, x)
                             for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+            # save the initialized variables in couchdb
 
             wdoc = {'w': convertToJSON(self.weights)}
             bdoc = {'b': convertToJSON(self.biases)}
@@ -78,11 +87,32 @@ class NNInvoker(object):
             doc1 = self.db.get(functionCount)
             print(doc1['count'])
 
+    def trainNN(self):
+        #this method will run all the functions
+        actIds = []
+        for x in range(self.parallelism):
+            actid = self.invokeNN()
+            actIds.append(actid)
 
-        # save the initialized variables in couchdb
+        return actIds
 
-        #invoke the NN functions according to the parallizm
+    def invokeNN(self):
+        #APIHOST = subprocess.check_output("wsk property get --apihost", shell=True).split()[2]
+        #AUTH_KEY = subprocess.check_output("wsk property get --auth", shell=True).split()[2]
+        #NAMESPACE = subprocess.check_output("wsk property get --namespace", shell=True).split()[2]
+        NAMESPACE = os.environ.get('__OW_NAMESPACE')
+        user_pass = os.environ.get('__OW_API_KEY').split(':')
+        ACTION = 'digitnn'
+        PARAMS = {'layers': self.sizes, 'epochs': self.epochs, 'eta':self.eta, 'mini_batch_size': self.mini_batch_size}
+        BLOCKING = 'false'
+        RESULT = 'true'
+        APIHOST = 'http://172.17.0.1:8888'
+        url = APIHOST + '/api/v1/namespaces/' + NAMESPACE + '/actions/' + ACTION
+
+        response = requests.post(url, json=PARAMS, params={'blocking': BLOCKING, 'result': RESULT}, auth=(user_pass[0], user_pass[1]))
+        return response.text
 
 def main(args):
     nninvoker = NNInvoker(args)
-    return {"LastDB": 'done'}
+    testre = nninvoker.trainNN()
+    return {"LastDB": testre}
